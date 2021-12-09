cd9f3e9b0102db2edb8c00c2fdc8994d741a07b5
Tharuk Renuja <90763454+TharukRenuja@users.noreply.github.com>
2021-12-09 13:27:01 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,13 +19,13 @@ BOT_TOKEN = "2146361275:AAF3mgx16FEFiw8ug6IHyXoCZlsrMKyu-e0"
 
 DB_URI = "mongodb+srv://erichdaniken:erichdaniken@cluster0.7enua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-USER_SESSION ="AQBZidX6xpS7DlheXbgjAgjnTfDl9jWSdOtTp-F0DZKdD-FEs0lkCwcNhvb3S5VvwaoV47yqoq5JGux4SwYcb577h3PrPK4Wjwrgktq9l4dtBKWCF8_25SOtHpCz2RicN6KV3B_66ZcvuL_IH6TEfBmXDWNb_IZfsAa1OYCSh_s8A8OQiMjnBhX6UwPiwfeZRMweeAul-rdspd-uvXSTxAarwSB3kTXdPbFkBejvNwqWC6iYT2V9EdyX51S9bVxm5HGGVk6LCTkNNj1fMNb5es7pYOaxFBcsLWbzZ1xC-V7UCixFPRRifaDVbLHOFo-GXZrJg_9nvPrY53xB5aVHT70oe6AmBgA
+USER_SESSION = "AQBZidX6xpS7DlheXbgjAgjnTfDl9jWSdOtTp-F0DZKdD-FEs0lkCwcNhvb3S5VvwaoV47yqoq5JGux4SwYcb577h3PrPK4Wjwrgktq9l4dtBKWCF8_25SOtHpCz2RicN6KV3B_66ZcvuL_IH6TEfBmXDWNb_IZfsAa1OYCSh_s8A8OQiMjnBhX6UwPiwfeZRMweeAul-rdspd-uvXSTxAarwSB3kTXdPbFkBejvNwqWC6iYT2V9EdyX51S9bVxm5HGGVk6LCTkNNj1fMNb5es7pYOaxFBcsLWbzZ1xC-V7UCixFPRRifaDVbLHOFo-GXZrJg_9nvPrY53xB5aVHT70oe6AmBgA
 
-BOT_NAME ="BOT_NAME", "🅷🅴🆇🅸🅽🅹 ☑️"
+BOT_NAME = "🅷🅴🆇🅸🅽🅹 ☑️"
 
-ADMIN_USERNAME = "ADMIN_USERNAME", "tifyny"
+ADMIN_USERNAME = "tifyny"
 
-GROUP_USERNAME = "GROUP_USERNAME", "Pirateslkmovies"
+GROUP_USERNAME = "Pirateslkmovies"
 
 VERIFY = {}
 
